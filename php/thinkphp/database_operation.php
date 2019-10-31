fa83b4811e84c665b80c36354e3d4073a9a88c52
chenweiwen <yy16@ai-guanjia.com>
2019-10-31 17:31:04 +0800

    think php raw query

--- php/thinkphp/database_operation.php
+++ php/thinkphp/database_operation.php
@@ -5,3 +5,9 @@ dump(M('User_adress')->_sql());exit();
 
 //order by in thinkphp3.1
 ->order("`default` DESC");// watch the '`' symbol. if this symbol does not exist, an error will occur.
+
+#Raw query
+$sql = 'select a.supply_id,a.yuyue_time,a.end_time,a.status,b.name 
+from pigcms_appoint_supply a left join pigcms_merchant_workers b on (a.worker_id = b.merchant_worker_id) 
+where a.order_id = ' . $order_id . ' order by order_time desc ';
+$now_supplys = D('Appoint_supply')->query($sql);
