e7f72e0917901f2ef4172a311cd5ce8c49c91f8d
wayne <570063013@qq.com>
2020-07-18 11:53:32 +0800

    adding a python common_knowledge and about passing GFW on windows's CMD

---
+++ python/common_knowledge.py
@@ -0,0 +1,9 @@
+# 1. return True or False
+def return_true_or_false():
+    # True would be returned.
+    return False or True
+
+
+# 2. pythonic way to return generated list
+def py_way_return_gen_list():
+    return
---
+++ windows/pass_GFW_on_CMD.md
@@ -0,0 +1,3 @@
+### pass great fire wall on cmd on windows
+setting up environment variables like this:
+![avatar](images/env_variables.png)
